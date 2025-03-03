cbdbaf0781ec6714fb0a499dd9a0ddf68ce3c1af
Vemas Bagus <masbrovemas@gmail.com>
2025-03-03 12:11:29 +0700

    Praktikum 2.1, minggu 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,21 +11,23 @@ class UserController extends Controller
     public function index()
     {
         //tambah data user dengan Elequent Model
-         $data = [
-            'level_id' => 2,
-            'username' => 'Manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-       ];
-        UserModel::create($data); 
+    //      $data = [
+    //         'level_id' => 2,
+    //         'username' => 'Manager_tiga',
+    //         'nama' => 'Manager 3',
+    //         'password' => Hash::make('12345'),
+    //    ];
+    //     UserModel::create($data); 
 
         // $data = [
         //     'nama' => 'Pelanggan Pertama'
         // ];
         // UserModel::where('username', 'customer-1')->update($data);
 
-        //coba akses model UserModel
-        $users = UserModel::all(); //ambil semua data dari tabel m_user
+        
+        $users = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404); 
+        }); 
         return view('user', ['data' => $users]);
     }
 }
