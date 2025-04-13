3faca673fbdd54d8b6364ba110518249d7de4465
Vemas Bagus <masbrovemas@gmail.com>
2025-04-13 20:22:43 +0700

    jobsheet8,praktikum3

--- routes/web.php
+++ routes/web.php
@@ -142,6 +142,7 @@ Route::middleware(['auth'])->group(function () { // artinya semua route di dalam
             Route::get('/import', [BarangController::class, 'import']);
             Route::post('/import_ajax', [BarangController::class, 'import_ajax']);
             Route::get('/export_excel', [BarangController::class, 'export_excel']);
+            Route::get('/export_pdf', [BarangController::class, 'export_pdf']);
             Route::get('/{id}/show_ajax', [BarangController::class, 'show_ajax']);
             Route::delete('/{id}', [BarangController::class, 'destroy']);
         });
